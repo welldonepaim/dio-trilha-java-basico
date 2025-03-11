34c44eb0319b13a73e1348b166754824f9f14b4f
Willian Paim <wpsilva7@ucs.br>
2025-03-10 21:57:08 -0300

    conhecendo variavies

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 # dio-trilha-java-basico
 repositório para execicíos de java básico para DIO
-Aprendendo sobre váriaveis
-tipos primitivos
-ESTOU UTILIZANDO NO LINU-----===== 
 
-Willian testando
+
---
+++ aprendnedo-operadores/src/TiposdeVariavies.java
@@ -0,0 +1,12 @@
+public class TiposdeVariavies {
+    public static void main(String[] args) {
+        String palavra="textos ";
+        int num=12;
+        float flut=12.0f;
+        double doubl=454.22;
+        boolean bool=true;
+        final String nome="willian";
+       // nome="joao";
+
+    }
+}
