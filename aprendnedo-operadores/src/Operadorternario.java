d9a95bb62099952948ee2e6e2c260084f119489f
Willian Paim <wpsilva7@ucs.br>
2025-03-12 22:04:14 -0300

    aprendendo operadores

---
+++ aprendnedo-operadores/src/Operadorternario.java
@@ -0,0 +1,11 @@
+public class Operadorternario {
+    public static void main(String[] args) {
+        int a,b;
+        a=1;
+        b=2;
+        String resultado=a==b?"verdadeiro":"falso";
+        System.out.println(resultado);
+        }
+    }
+}
+
